c522172a7dd4be3b9d65bf47371c6ae4ed18ad06
Sirdayne <mussinyernar@gmail.com>
2018-01-20 22:16:48 +0600

    added post, put, delete requests

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+
 let app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 let artists = [
   {
@@ -18,7 +22,7 @@ let artists = [
 
 app.get('/', function(req, res){
   res.send('hello API!')
-});
+})
 
 app.get('/artists', (req, res) => {
   res.send(artists)
@@ -29,6 +33,25 @@ app.get('/artists/:id', (req, res) => {
   res.send(artist)
 })
 
+app.post('/artists', (req, res) => {
+  let artist = {
+    id: Date.now(),
+    name: req.body.name
+  }
+  artists.push(artist)
+  res.send(`200 - OK: ${artist.name} added`)
+})
+
+app.put('/artists/:id', (req, res) => {
+  let artist = artists.find(a => a.id === Number(req.params.id))
+  artist.name = req.body.name
+  res.sendStatus(200)
+})
+
+app.delete('/artists/:id', (req, res) => {
+  artists = artists.filter(a => a.id !== Number(req.params.id))
+  res.sendStatus(200)
+})
 
 app.listen(3012, function(){
   console.log('APP started')
